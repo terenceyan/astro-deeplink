f351350e89fd209e7186703e3dc230f3bae53a14
Terence <tyan@pinterest.com>
2024-02-29 13:16:39 -0500

    Change to use netlify redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,5 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
-  redirects: {
-    '/': {
-      status: 302,
-      destination: 'www.google.ca'
-    }
-  }
+  adapter: netlify()
 });
